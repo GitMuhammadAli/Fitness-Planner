0deafafcddb17de44199e36d042d288ad764cb18
malingxoft <malingxoft@gmail.com>
2024-08-27 03:05:33 +0500

    commit

--- client/src/Api/index.js
+++ client/src/Api/index.js
@@ -1,39 +1,39 @@
 import axios from "axios";
 
+const BASE_URL = 'http://localhost:9000';
+
 export const fetchCategories = () => {
-  return axios.get('http://localhost:9000/workoutsCategory', { withCredentials: true });
+  return axios.get(`${BASE_URL}/workoutsCategory`, { withCredentials: true });
 };
 
 export const fetchScheme = () => {
-  return axios.get('http://localhost:9000/workoutsSchemes', { withCredentials: true });
+  return axios.get(`${BASE_URL}/workoutsSchemes`, { withCredentials: true });
 };
 
 export const fetchIndividualTypeWorkouts = () => {
-  return axios.get('http://localhost:9000/individualType', { withCredentials: true });
+  return axios.get(`${BASE_URL}/individualType`, { withCredentials: true });
 };
 
 export const fetchBroSplitTypeWorkouts = () => {
-  return axios.get('http://localhost:9000/broSplitWorkouts', { withCredentials: true });
+  return axios.get(`${BASE_URL}/broSplitWorkouts`, { withCredentials: true });
 };
 
 export const fetchBodybuilderTypeSplitWorkouts = () => {
-  return axios.get('http://localhost:9000/bodybuilderSplitWorkouts', { withCredentials: true });
+  return axios.get(`${BASE_URL}/bodybuilderSplitWorkouts`, { withCredentials: true });
 };
 
 export const fetchUpperLowerTypeWorkouts = () => {
-  return axios.get('http://localhost:9000/upperLowerWorkouts', { withCredentials: true });
+  return axios.get(`${BASE_URL}/upperLowerWorkouts`, { withCredentials: true });
 };
 
 export const fetchExercises = () => {
-  return axios.get('http://localhost:9000/exercises', { withCredentials: true });
+  return axios.get(`${BASE_URL}/exercises`, { withCredentials: true });
 };
 
 export const senddata = (data) => {
-  return axios.post('http://localhost:9000/data', data, { withCredentials: true });
+  return axios.post(`${BASE_URL}/data`, data, { withCredentials: true });
 };
 
-
-
 export const ProceedData = (data) => {
-  return axios.post('http://localhost:9000/data', data, { withCredentials: true });
+  return axios.post(`${BASE_URL}/data`, data, { withCredentials: true });
 };
